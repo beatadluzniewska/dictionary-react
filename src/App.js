cc36df3e9580d353d39150f53461ba1d429d782d
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2024-01-01 19:30:20 +0100

    css and image added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import plakat from "./Plakat_SaM-2020.jpg";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <img src={plakat} alt="plakat" className="App-logo img-fluid" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
