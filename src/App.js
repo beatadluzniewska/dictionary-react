7ad1de14faca9c927ad272d847fc6dd99c54e2d5
beatadluzniewska <109465887+beatadluzniewska@users.noreply.github.com>
2024-01-04 22:50:14 +0100

    search function added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import plakat from "./Plakat_SaM-2020.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={plakat} alt="plakat" className="App-logo img-fluid" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div className="container">
+                <header className="App-header">
+                    <img
+                        src={plakat}
+                        alt="plakat"
+                        className="App-logo img-fluid"
+                    />
+                </header>
+                <main>
+                    <Dictionary />
+                </main>
+                <footer className="App-footer">
+                    <small>Coded by Beata Dluzniewska</small>
+                </footer>
+            </div>
         </div>
     );
 }
